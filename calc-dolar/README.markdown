762c748e971de13fd025c07e6389c6988f64ab7d
Laura <79175922+lauramrqs@users.noreply.github.com>
2021-04-01 19:37:00 -0300

    Add files via upload

---
+++ calc-dolar/README.markdown
@@ -0,0 +1,5 @@
+# Calc Dolar
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/laura-marques/pen/xxgxZZy](https://codepen.io/laura-marques/pen/xxgxZZy).
+
+
---
+++ calc-dolar/src/script.js
@@ -0,0 +1,9 @@
+ 
+
+var valorDol = parseFloat(prompt("Qual o valor do produto em dolarés?"));
+
+valorDolFinal = valorDol*5.51
+
+var valorDolFinalDec= parseFloat(valorDolFinal).toFixed(2);
+
+alert(valorDolFinalDec)
